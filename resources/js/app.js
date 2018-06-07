129d25aba0ce7eb96d1a13fc020158670a21f1a9
sorasenpai <mo-_-karim@live.fr>
2018-06-07 15:29:37 +0000

    Adding Sub slider

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-              var owl = $('.owl-carousel');
+              var owl = $('#header-carousel');
               owl.owlCarousel({
                 loop: true,
                 dots:false,
@@ -25,28 +25,56 @@ $(document).ready(function() {
     
     
     /*Change the sticky navbar*/
-    $('.js--section--popular').waypoint(function(direction) {
-       if(direction == "down")
-       {
-           setTimeout(function() {
-                $('nav').addClass('sticky');
-              }, 500);
-           
-       }else
-       {
-           setTimeout(function() {
-                $('nav').removeClass('sticky');
-              }, 500);
-       }
-    },{
-        offset: '5%'
+//    $('.js--section--popular').waypoint(function(direction) {
+//       if(direction == "down")
+//       {
+//           setTimeout(function() {
+//                $('nav').addClass('sticky');
+//              }, 500);
+//           
+//       }else
+//       {
+//           setTimeout(function() {
+//                $('nav').removeClass('sticky');
+//              }, 500);
+//       }
+//    },{
+//        offset: '5%'
+//    });
+//    
+    //Check if navabar has extended class
+    $(".menu-icon").click(function(){
+        var searchgroup = ".top-menu-standard form.search-group";
+        var menu = ".top-menu-standard > div > ul";
+        if(!$(searchgroup).hasClass("extended") && !$(menu).hasClass("extended")){
+            $(searchgroup).addClass("extended");
+            $(menu).addClass("extended");
+        }else {
+            
+            $(searchgroup).removeClass("extended");
+            $(menu).removeClass("extended");
+        }
+    })
+    
+    $('#carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        dots:false,
+        nav:true,
+        navText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+        autoplay:true,
+        smartSpeed: 1000,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:4
+            }
+        }
     });
-    
-    
-    
-//    var waypoints = $('.js--section--popular').waypoint(function(direction) {
-//      notify(this.element.id + ' hit 25% from top of window') 
-//    }, {
-//      offset: '25%'
-//    })
 });
